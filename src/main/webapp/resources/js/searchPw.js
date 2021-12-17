d565ffe7579d4d105086f0eefb09623cdf24b97c
dong-hoon <ehdgns874@gmail.com>
2021-12-17 16:36:34 +0900

    12월 17일 커밋

---
+++ src/main/webapp/resources/js/searchPw.js
@@ -0,0 +1,62 @@
+const signUpCheckObj = {
+    "email": false,
+    "name": false,
+    "pw": false,
+    "pw1": false
+}
+
+
+/* 이메일 유효성 중복확인 */
+
+$("#searchPw-email").on("input", function () {
+
+    const regExp = /^[\w]{4,}@[\w]+(\.[\w]+){1,3}$/;
+    const inputEmail = $(this).val();
+
+    if (inputEmail.length == 0) {
+        $("#checkEmail").text("");
+        signUpCheckObj.email = false;
+    } else if (regExp.test(inputEmail)) {
+
+        $.ajax({
+
+            url: "emailDupCheck",
+            type: "GET",
+            data: { "inputEmail": inputEmail },
+            success: function (result) {
+                if (result != 0) {
+                    $("#checkEmail").text("등록된 이메일입니다.").css("color", "green");
+                    signUpCheckObj.email = true;
+
+			
+                } else {
+                    $("#checkEmail").text("등록되지 않은 이메일입니다.").css("color", "red");
+                    signUpCheckObj.email = false;
+                }
+            },
+            error: function (request, status, error) {
+
+
+            },
+            complite: function () {
+
+            }
+        });
+
+
+		
+    } else {
+        $("#checkEmail").text("잘못된 이메일 형식입니다.").css("color", "red");
+        signUpCheckObj.email = false;
+    }
+
+});
+function emailput(){
+	if(signUpCheckObj.email == true){
+		
+		alert("전송완료");
+	}else if(signUpCheckObj.email == false){
+		alert("이메일을 확인해주세요")
+	}
+	
+}
