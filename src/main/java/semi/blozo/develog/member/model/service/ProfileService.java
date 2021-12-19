4145c57266b1bc24b0df99dc15af7a40e2d8efed
JooDB <joodb0126@gmail.com>
2021-12-19 14:31:05 +0900

    211219 패키지명 변경

---
+++ src/main/java/semi/blozo/develog/member/model/service/ProfileService.java
@@ -0,0 +1,14 @@
+package semi.blozo.develog.member.model.service;
+
+import static semi.blozo.develog.common.JDBCTemplate.*;
+
+import semi.blozo.develog.member.model.dao.ProfileDAO;
+
+
+public class ProfileService {
+	
+	private ProfileDAO dao = new ProfileDAO();
+	
+	
+	
+}
