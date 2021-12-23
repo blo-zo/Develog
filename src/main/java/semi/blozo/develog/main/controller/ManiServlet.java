6c17bb0eaaad23fcffec6dc06dbec0f6a8689dcc
dong-hoon <ehdgns874@gmail.com>
2021-12-23 18:36:12 +0900

    자동 슬라이드 메인페이지 카드, 캐러셀 데이터 삽입 쿼리문 수정

--- src/main/java/semi/blozo/develog/main/controller/ManiServlet.java
+++ src/main/java/semi/blozo/develog/main/controller/ManiServlet.java
@@ -17,12 +17,18 @@ public class ManiServlet extends HttpServlet{
 		protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 			// 메인 페이지 요청과 동시에 블로그 전체 게시글 수 조회 후 카드 생성
 			
-			PostService service = new PostService();
-			//List<Post> postListAll = service.selectBlogPostListAll();
-			
-			
-			String path = "/WEB-INF/views/common/main.jsp";
-			req.getRequestDispatcher(path).forward(req, resp);
+			try {
+				
+				PostService service = new PostService();
+				List<Post> postListAll = service.selectPostListAll();
+				//화면출력
+				System.out.println(postListAll);
+				req.setAttribute("postListAll", postListAll);
+				String path = "/WEB-INF/views/common/main.jsp";
+				req.getRequestDispatcher(path).forward(req, resp);
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
 			
 
 			
