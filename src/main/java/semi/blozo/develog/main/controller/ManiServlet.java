9a51c0696b9b9fc09675bed1d03345d91c774b81
dong-hoon <ehdgns874@gmail.com>
2021-12-23 14:16:01 +0900

    유동훈 enquiryView.css 경로 수정 maincontroller 작업 커밋

--- src/main/java/semi/blozo/develog/main/controller/ManiServlet.java
+++ src/main/java/semi/blozo/develog/main/controller/ManiServlet.java
@@ -7,18 +7,39 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-@WebServlet("/main")
+@WebServlet("/main/*")
 public class ManiServlet extends HttpServlet{
 		@Override
 		protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+			
+			
+			
 			//메인페이지 요청 시 로직처리
 			/*
 			 * String main = req.getContextPath()+"/resources/css/mainPage.css";
 			 * req.setAttribute("main", main);
 			 */
+
+			String method = req.getMethod();
+			
+			String uri = req.getRequestURI();
+			String contextPath = req.getContextPath();
+			String command = uri.substring( (contextPath + "/main/").length());
+			
+			
 			
 			String path = "/WEB-INF/views/common/main.jsp";
 			req.getRequestDispatcher(path).forward(req, resp);
+			if(command.equals("trend")) {
+				
+			}
+
+			
+		} 
+		@Override
+		protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doGet(req, resp); // do get 으로 post 요청처리
 		
 		}
+		
 }
